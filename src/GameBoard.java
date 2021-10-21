facf3e6bbed46be25fa918af30d969dc8bcd6645
xantippas <xantippa.hogberg@gmail.com>
2021-10-21 12:39:42 +0200

    Initial commit

---
+++ src/GameBoard.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class GameBoard extends JFrame {
+
+    GameBoard(){
+        
+    }
+
+    public static void main(String[] args) {
+        GameBoard playGame = new GameBoard();
+    }
+}
