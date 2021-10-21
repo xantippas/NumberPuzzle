1b885994cff1bdd6e20ac5a133bf5feff6f6b614
xantippas <xantippa.hogberg@gmail.com>
2021-10-21 13:03:13 +0200

    added skeleton for version 1 of the Game Board

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,9 +1,30 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GameBoard extends JFrame {
 
-    GameBoard(){
+    JPanel mainPanel = new JPanel();
+    JPanel topPanel = new JPanel();
+    JPanel gamePanel = new JPanel();
+    JPanel bottomPanel = new JPanel();
+    JButton newGameButton = new JButton("New Game");
+    JLabel rubrik = new JLabel("Number Puzzle Game");
 
+    GameBoard(){
+        mainPanel.setLayout(new BorderLayout());
+        topPanel.add(rubrik);
+        gamePanel.setLayout(new GridLayout(4,4));
+        bottomPanel.add(newGameButton);
+
+        mainPanel.add(topPanel, BorderLayout.NORTH);
+        mainPanel.add(gamePanel, BorderLayout.CENTER);
+        mainPanel.add(bottomPanel, BorderLayout.SOUTH);
+        add(mainPanel);
+
+        setVisible(true);
+        setSize(420,420);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
