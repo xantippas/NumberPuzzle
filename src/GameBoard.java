7b055ee03bdf82c471b61738cd251ba4daeb6647
Sofya Dziamidava <sofya.dziamidava@gmail.com>
2021-10-21 14:06:12 +0200

    adding createRandomLabels method

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class GameBoard extends JFrame {
 
@@ -9,6 +10,8 @@ public class GameBoard extends JFrame {
     JPanel bottomPanel = new JPanel();
     JButton newGameButton = new JButton("New Game");
     JLabel rubrik = new JLabel("Number Puzzle Game");
+   // JButton[][] gameNumber = new JButton[4][4];
+
 
     GameBoard(){
         mainPanel.setLayout(new BorderLayout());
@@ -27,7 +30,37 @@ public class GameBoard extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
+    public static void createRandomLabels(){
+        JLabel[] gameNumber = new JLabel[15];
+        /*
+        for (int i = 0; i < 15; i++) {
+            double number = Math.random()*(15-1)+1;
+            String numberStr = Double.toString(number);
+            int numberInt = Integer.parseInt(numberStr);
+            gameNumber[i] = new JLabel(Integer.toString(numberInt));
+            System.out.println(gameNumber[i].getText());
+        }
+
+         */
+
+        Random random = new Random();
+            for (int i = 0; i < 15; i++) {
+                int index = random.nextInt(15)+1;
+                gameNumber[i] = new JLabel(Integer.toString(index));
+                for (int j = 0; j < i; j++) {
+                if (Integer.parseInt(gameNumber[j].getText()) != index){
+                    gameNumber[i] = new JLabel(Integer.toString(index));
+                } else if (Integer.parseInt(gameNumber[j].getText()) == index){
+
+                }
+                }
+                System.out.println(gameNumber[i].getText());
+        }
+    }
+
     public static void main(String[] args) {
         GameBoard playGame = new GameBoard();
+        GameBoard.createRandomLabels();
+
     }
 }
